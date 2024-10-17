4cf3cc466f4db5c43e664324b835d70b325d0edf
이혜빈 <bin020129@gmail.com>
2024-10-17 17:05:01 +0900

    241017

--- RunView.swift
+++ RunView.swift
@@ -73,10 +73,17 @@ struct RunView: View {
 
 // 러닝 모임 개설 페이지
 struct RunCreateView: View {
-    @State private var RunCreateText: String = "" // 제목 저장하는 상태 변수
-    @State private var SelectedDate = Date() // 날짜 저장하는 상태 변수
-    @State private var SelectedLocation = "서울" // 위치 선택하는 상태 변수
-    @State private var SelectedSubLocation = "" // 세부 위치 선택하는 상태 변수
+    @State private var RunCreateText: String = "" // 제목 저장하는 변수
+    @State private var SelectedDate = Date() // 날짜 저장하는 변수
+    @State private var SelectedLocation = "서울" // 위치 선택하는 변수
+    @State private var SelectedSubLocation = "" // 세부 위치 선택하는 변수
+    @State private var CourseLength: Double = 5.0 // 코스 길이를 저장하는 변수
+    @State private var SelectedMinutes: Int = 4 // 분 초기값
+    @State private var SelectedSeconds: Int = 0 // 초 초기값 (0 또는 30)
+    //    @State private var runningPace: Double = 4.0 // 기본 러닝 페이스 (4분)
+    @State private var RunnerCount = "2" // 인원수 선택하는 변수
+    @State private var RunningTime = "4 : 00" // 러닝 페이스 변수
+    @State private var TextAdd: String = "" // 상세 설명 추가 변수
     let locations = ["서울", "부산", "대구"/*, "인천", "광주", "대전", "울산", "세종", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"*/]
     let subLocations = [
         "서울": ["강서구", "강남구", "종로구"],
@@ -84,8 +91,15 @@ struct RunCreateView: View {
         "대구": ["중구", "남구", "수성구"],
         // 필요에 따라 다른 도시와 세부 지역 추가하면 됨
     ]
-    @State private var RunnerCount = "2" // 인원수 선택하는 상태 변수
+    
     let RunCount = ["2", "3", "4", "5", "6", "7", "8"]
+    var RunTime = ["4 : 00", "4 : 30", "5 : 00", "5 : 30", "6 : 00", "6 : 30", "7 : 00", "7 : 30", "8 : 00"]
+    
+    // 분과 초에 대한 선택값을 제공
+    let minutes = Array(4...8) // 4분에서 8분까지 선택 가능
+    let seconds = [0, 30] // 초는 0초 또는 30초 선택 가능
+    
+    
     
     var body: some View {
         // 스크롤뷰로 전체 감싸기
@@ -116,6 +130,8 @@ struct RunCreateView: View {
                     .padding(.horizontal, 22)
                     .padding(.leading, 5)
                     .padding(.top, 18)
+                
+                // 러닝 장소 피커
                 HStack {
                     Picker("도시를 선택하세요", selection: $SelectedLocation) {
                         ForEach(locations, id: \.self) { location in
@@ -136,21 +152,6 @@ struct RunCreateView: View {
                 }
                 
             }
-            
-            //            // 참여 인원
-            //            HStack {
-            //                Text("참여 인원")
-            //                    .padding(.horizontal, 22)
-            //                    .frame(alignment: .leading)
-            //
-            //                Picker("인원을 선택하세요", selection: $RunnerCount) {
-            //                    ForEach(RunCount, id: \.self) { count in
-            //                        Text(count)
-            //                    }
-            //                }
-            //                .pickerStyle(WheelPickerStyle())
-            //                Spacer()
-            //            }
             
             
             // 참여 인원
@@ -180,9 +181,89 @@ struct RunCreateView: View {
                     .padding(.horizontal, 22)
                 }
                 Spacer()
+                
+            } // HStack 참여인원
+            
+            
+            // 코스 길이 슬라이더
+            VStack {
+                Slider(value: $CourseLength, in: 0...20, step: 1.0)
+                    .padding()
+                
+                // 슬라이더 범위 표시
+                HStack {
+                    Text("0 km") // 최소값
+                    Spacer()
+                    Text("20 km") // 최대값
+                }
+                .padding(.horizontal)
+                
+                // 선택된 코스 길이 표시
+                Text("코스 길이: \(CourseLength, specifier: "%.1f") km")
+                    .font(.headline)
+                    .padding()
             }
             
-            // 아무말
+            
+            // 러닝 페이스 Stepper
+            HStack {
+                Text("러닝 페이스")
+                    .padding(.horizontal, 22)
+                
+                // 참여 인원 목록 상자
+                Menu {
+                    ForEach(RunTime, id: \.self) { Time in
+                        Button(action: {
+                            RunningTime = Time// 선택된 인원 수 저장
+                        }) {
+                            Text(Time)
+                        }
+                    }
+                } label: {
+                    HStack {
+                        Text(RunningTime) // 현재 선택된 인원 수
+                            .foregroundColor(.primary)
+                        Spacer()
+                        Image(systemName: "chevron.up") // 화살표 아이콘
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 22)
+                }
+                Spacer()
+                
+            } // HStack 러닝 페이스
+            
+            // 상세 설명 추가
+            VStack {
+                TextField("상세 설명 추가", text: $TextAdd)
+                    .frame(height: 200)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 22)
+                    .padding(.top, 10)
+                
+            }
+            
+            // 개설하기 버튼
+            VStack {
+                Button(action: {
+                    print("버튼 누르면 실행")
+                }) {
+                    Text("개설하기")
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 80)
+                        .padding(.vertical, 15)
+                        .background(Color(.systemTeal))
+                        .cornerRadius(10)
+
+                }
+                
+                
+            }
+            
         } // ScrollView 전체
         .navigationBarTitle("러닝 모임 개설") // 네비게이션바 제목
         .navigationBarTitleDisplayMode(.inline) // 네비게이션바 글씨 스타일
---
+++ Splash.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIView.swift
+//  MoveApp
+//
+//  Created by 이혜빈 on 10/17/24.
+//
+
+import SwiftUI
+
+
+// 스플래시 화면
+struct SplashScreen: View {
+    @Binding var isActive: Bool // 권한 넘겨주기 위해 binding
+    
+    var body: some View {
+        ZStack{
+            Color.teal.opacity(0.5) // 스플래시 전체 색상
+            
+            VStack{
+                Image(systemName: "figure.run") // 로고 이미지 run
+                    .resizable() // 이미지 수정 허용
+                    .scaledToFit() // 주어진 프레임에 맞게 비율 유지하며 조정
+                    .frame(width: 150, height: 150) // 로고 프레임 크기 설정
+            }
+            // 뷰가 화면에 나타날 때 실행해야 하는 코드
+            .onAppear {
+                // 2초 후 메인 뷰로 전환
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) { withAnimation {
+                    isActive = true
+                }
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+} // SplachScreen 전체
